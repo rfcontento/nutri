c918bc6233a93fa4502af5ee3ef22a0793879239
Ronny Contento <ronnyfer95@gmail.com>
2025-04-11 19:57:14 -0500

    Start connect server traccar

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:component/theme/global_theme_light.dart';
 import 'package:driver/ui/page/splash/splash_screen.dart';
 import 'package:driver/ui/routes/route_page.dart';
 import 'package:driver/ui/routes/route_provider.dart';
+import 'package:driver/ui/util/device_info_id.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:traccar_flutter/entity/traccar_configs.dart';
+import 'package:traccar_flutter/traccar_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initConfigTracking();
   runApp(const MyApp());
 }
 
