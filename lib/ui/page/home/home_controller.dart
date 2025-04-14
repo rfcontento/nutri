4294701837b48868aad244a895fe6da03a5bf7ec
Ronny Contento <ronnyfer95@gmail.com>
2025-04-14 14:12:16 -0500

    Move camera pip

--- lib/ui/page/home/home_controller.dart
+++ lib/ui/page/home/home_controller.dart
@@ -1,17 +1,22 @@
+import 'dart:async';
 import 'dart:ui' as lib_ui;
 
 import 'package:driver/domain/entities/location_model.dart';
 import 'package:driver/ui/controller/location_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeController with ChangeNotifier {
+  final LocationProvider locationProvider = LocationProvider();
   LatLng? mapPosition;
   GoogleMapController? _mapController;
   GoogleMapController get mapController => _mapController!;
   Map<MarkerId, Marker>? markers = {};
   bool? _isOpenCard = false;
+  StreamSubscription<Position>? locationSubscription;
+
 
   bool get isOpenCard => _isOpenCard!;
 
@@ -26,28 +31,13 @@ class HomeController with ChangeNotifier {
   }
 
   initHome() async {
-    final location = await LocationProvider().initLocationService(timeoutTime: 5);
+    final location = await locationProvider.initLocationService(timeoutTime: 5);
     if (location.status == LocationStatus.success) {
       mapPosition = location.latLng;
-      markerDriver();
-      notifyListeners();
+      _startListeningLocation();
     }
   }
 
-  void markerDriver() {
-    Marker? marker = markers![const MarkerId("id_marker")];
-    createMarker("assets/icons/pin.png").then((pinMarker) {
-
-      if (marker == null) {
-        markers![const MarkerId("id_marker")] = Marker(
-          markerId: const MarkerId("id_marker"),
-          position: LatLng(mapPosition!.latitude, mapPosition!.longitude),
-          icon: pinMarker
-        );
-        notifyListeners();
-      }
-    });
-  }
 
   Future<BitmapDescriptor> createMarker(String urlAssets) async {
     final Uint8List markerIcon = await getBytesFromAsset(urlAssets, 130);
@@ -74,4 +64,37 @@ class HomeController with ChangeNotifier {
   void initMapController(GoogleMapController controller) {
     _mapController = controller;
   }
+
+  void _startListeningLocation() {
+    locationSubscription = locationProvider.notifications.listen((Position position) {
+      addMarker(LatLng(position.latitude, position.longitude));
+      moveCamera(LatLng(position.latitude, position.longitude));
+    });
+  }
+
+  void moveCamera(LatLng position) {
+    _mapController?.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(target: position, zoom: 15.0),
+    ));
+  }
+
+
+
+  void addMarker(LatLng position) {
+    markers ={};
+    Marker? marker = markers![const MarkerId("id_marker")];
+    createMarker("assets/icons/pin.png").then((pinMarker) {
+
+      if (marker == null) {
+        markers![const MarkerId("id_marker")] = Marker(
+            markerId: const MarkerId("id_marker"),
+            position: LatLng(position.latitude, position.longitude),
+            icon: pinMarker
+        );
+        notifyListeners();
+      }
+    });
+  }
+
+
 }
